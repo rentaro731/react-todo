8d1331c00a69574ebf96bd842a01fa4b6c2f0720
rentaro731 <horen731731@gmail.com>
2025-07-05 21:31:19 +0900

    TodoDataとTodoList合併
    TodoDataコンポーネントのデータをTodoListに移行

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,8 +1,30 @@
 import { Radio } from "./Radio.jsx";
 import { TodoItem } from "./TodoItem.jsx";
-import { TodoData } from "./TodoData.jsx";
+import { useState } from "react";
 export function TodoList() {
-  const todos = TodoData();
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: "Javascriptの基礎",
+      date: "2024-01-01",
+      status: "作業中",
+      delete: "✖️",
+    },
+    {
+      id: 2,
+      title: "非同期処理",
+      date: "2024-01-02",
+      status: "作業中",
+      delete: "✖️",
+    },
+    {
+      id: 3,
+      title: "オブジェクト指向",
+      date: "2024-01-03",
+      status: "作業中",
+      delete: "✖️",
+    },
+  ]);
   return (
     <>
       <Radio options={["すべて", "作業中", "完了"]} />
