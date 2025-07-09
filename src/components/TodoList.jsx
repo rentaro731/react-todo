e9349c4d535b010b821fe736cebecdfe3090329b
rentaro731 <horen731731@gmail.com>
2025-07-09 22:15:09 +0900

    add-todo
    todoリストの追加機能を実装

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,30 +1,47 @@
 import { Radio } from "./Radio.jsx";
 import { TodoItem } from "./TodoItem.jsx";
-import { useState } from "react";
+import { useState, useRef } from "react";
 export function TodoList() {
-  const [todos, setTodos] = useState([
+  const todoData = [
     {
       id: 1,
       title: "Javascriptの基礎",
       date: "2024-01-01",
       status: "作業中",
-      delete: "✖️",
     },
     {
       id: 2,
       title: "非同期処理",
       date: "2024-01-02",
       status: "作業中",
-      delete: "✖️",
     },
     {
       id: 3,
       title: "オブジェクト指向",
       date: "2024-01-03",
       status: "作業中",
-      delete: "✖️",
     },
-  ]);
+  ];
+  const [todos, settodos] = useState(todoData);
+  const [taskName, setTaskName] = useState("");
+  console.log("タスク名", taskName);
+  const [newDate, setNewDate] = useState("");
+  console.log("期限", newDate);
+  const AddTodo = (e) => {
+    e.preventDefault();
+    settodos((todos) => [
+      ...todos,
+      {
+        id: todos.length + 1,
+        title: taskName,
+        date: newDate,
+        status: "作業中",
+      },
+    ]);
+
+    setTaskName("");
+    setNewDate("");
+  };
   return (
     <>
       <Radio options={["すべて", "作業中", "完了"]} />
@@ -44,15 +61,22 @@ export function TodoList() {
           ))}
         </tbody>
       </table>
-      <form id="form">
+      <>
         <input
           type="text"
           style={{ marginRight: "10px" }}
           placeholder="タスク名を入力"
+          value={taskName}
+          onChange={(e) => setTaskName(e.target.value)}
         />
-        <input type="date" style={{ marginRight: "10px" }} />
-        <button type="submit">追加</button>
-      </form>
+        <input
+          type="date"
+          style={{ marginRight: "10px" }}
+          value={newDate}
+          onChange={(e) => setNewDate(e.target.value)}
+        />
+        <button onClick={AddTodo}>追加</button>
+      </>
     </>
   );
 }
